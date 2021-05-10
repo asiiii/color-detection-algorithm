2e50fc43d594c9b387a87df0f653fd51fc5edaca
asiiii <73032891+asiiii@users.noreply.github.com>
2021-05-11 02:35:18 +0530

    Add files via upload

---
+++ color_detection.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+import pandas as pd
+
+# Reading the image with opencv
+img = cv2.imread('C:/Users/Asiri Ekanayake/PycharmProjects/ocellus-backend-node_OLD/uploads/CAP1507066802404860545.jpg')
+
+# declaring global variables (are used later on)
+clicked = False
+r = g = b = xpos = ypos = 0
+
+# Reading csv file with pandas and giving names to each column
+index = ["color", "color_name", "hex", "R", "G", "B"]
+csv = pd.read_csv('colors.csv', names=index, header=None)
+
+
+# function to calculate minimum distance from all colors and get the most matching color
+def getColorName(R, G, B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if (d <= minimum):
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+    return cname
+
+
+# function to get x,y coordinates of mouse double click
+def draw_function(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b, g, r, xpos, ypos, clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b, g, r = img[y, x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', draw_function)
+
+while (1):
+    cv2.imshow("image", img)
+    if (clicked):
+        # Creating text string to display( Color name and RGB values )
+        text = getColorName(r, g, b)
+        print(text)
+        clicked = False
+
+    # Break the loop when user hits 'esc' key
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
